87515c8df45c6d689fc0abfca5fb9d977f0fd8c6
Michael Heiniger <michael.heiniger89@protonmail.ch>
2021-12-08 22:08:58 +0100

    Upgrade AGP from 7.0.3 to 7.0.4
    Recommended for ArcticFox 2020.3.1 patch 4

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
     const val kotlin = "1.5.30"
 
     // Gradle plugins
-    const val androidGradlePlugin = "7.0.3"
+    const val androidGradlePlugin = "7.0.4"
     const val ktlintGradlePlugin = "10.1.0"
     const val detekt = "1.18.1"
     const val androidJUnit5Plugin = "1.7.1.1"
