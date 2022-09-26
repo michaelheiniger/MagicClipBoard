c7df6b0293c2e276c4d553f107ec8b702524d3b9
Michael Heiniger <michael.heiniger89@protonmail.ch>
2022-09-26 22:55:26 +0200

    Change theme colors

--- app/src/main/java/ch/qscqlmpa/magicclipboard/ui/theme/Color.kt
+++ app/src/main/java/ch/qscqlmpa/magicclipboard/ui/theme/Color.kt
@@ -11,9 +11,13 @@ val LightBlueDark = Color(0xFF007ac1)
 val LightBlue500 = Color(0xFF03a9f4)
 val LightBlueLight = Color(0xFF67daff)
 
-val OrangeDark = Color(0xFFc66900)
-val Orange500 = Color(0xFFff9800)
-val OrangeLight = Color(0xFFffc947)
+val AmberDark = Color(0xFFc68400)
+val Amber600 = Color(0xFFffb300)
+val AmberLight = Color(0xFFffe54c)
+
+val OrangeDark = Color(0xFFbb4d00)
+val Orange700 = Color(0xFFf57c00)
+val OrangeLight = Color(0xFFffad42)
 
 val GrayDark = Color(0xFF000000)
 val Gray900 = Color(0xFF212121)
